06ecb992c3c9d9cb36f8de4bc152c1a810c6fc3e
ArttuKuikka <arttu.juhani.kuikka@gmail.com>
2022-01-13 12:59:18 +0200

    final commit before release

--- Ruokalista.Android/MainActivity.cs
+++ Ruokalista.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Ruokalista.Droid
 {
-    [Activity(Label = "Ruokalista", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Ruokalista", Name = "com.ArttuKuikka.Ruokalista.MainActivity" , Icon = "@drawable/logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
